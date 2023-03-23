618a73d2861eb234425fe86938ae1407cfcc8893
quantumjot <code@arlowe.co.uk>
2023-03-23 15:06:41 -0400

    add Wr

--- octopuslite/writer.py
+++ octopuslite/writer.py
@@ -7,29 +7,40 @@ from skimage.io import imsave
 import numpy.typing as npt
 
 
-def write(
-    data: npt.NDArray,
-    path: os.PathLike,
-    *,
-    position: str = "001",
-    channel: Channels = Channels.GFP,
-    parser: MetadataParser = MetadataParser.OCTOPUS,
-) -> None:
+class Writer:
+    def __init__(
+        self,
+        path: os.PathLike,
+        *,
+        position: str = "001",
+        channel: Channels = Channels.GFP,
+        parser: MetadataParser = MetadataParser.OCTOPUS,
+    ) -> None:
+        """Writer for image data."""
+        self.path = Path(path)
+        self.position = position
+        self.parser = parser.value
+        self.channel = channel
+
+    def write(self, image: npt.NDArray, t: int, *, z: int = 0) -> None:
+        """Write a single frame of the data."""
+        params = {
+            "filename": "",
+            "position": self.position,
+            "channel": self.channel,
+            "time": int(t),
+            "z": z,
+        }
+        metadata = self.parser.from_dict(params)
+
+        filename = Path(self.path / metadata.to_filename()).with_suffix(".tif")
+        imsave(filename, image, check_contrast=False)
+
+
+def write(data: npt.NDArray, path: os.PathLike, **kwargs) -> None:
     """Write out image data in a format that can be read by DaskOctopus."""
 
-    parser = parser.value
-    path = Path(path)
+    writer = Writer(path, **kwargs)
 
     for t in range(data.shape[0]):
-        metadata = parser.from_dict(
-            {
-                "filename": "",
-                "position": position,
-                "channel": channel,
-                "time": int(t),
-                "z": 0,
-            }
-        )
-
-        filename = Path(path / metadata.to_filename()).with_suffix(".tif")
-        imsave(filename, data[t, ...], check_contrast=False)
+        writer.write(data[t, ...], t)
